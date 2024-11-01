17deb4a326c2a32af02e9e76e9bd8eaed09510f2
Laslo76 <162439161+Laslo76@users.noreply.github.com>
2024-11-01 08:39:41 +0300

    add db_depends

--- app/backend/db.py
+++ app/backend/db.py
@@ -3,11 +3,8 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 
 engine = create_engine("sqlite:///taskmanager.db", echo=True)
-
-session_local = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(bind=engine)
 
 
 class Base(DeclarativeBase):
     pass
-
-
