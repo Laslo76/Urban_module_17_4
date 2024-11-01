770829b00a8d937e43dc2a6f2da1a7f34cf02e78
Laslo76 <162439161+Laslo76@users.noreply.github.com>
2024-11-01 08:38:04 +0300

    edit db.py

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,1 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+
+engine = create_engine("sqlite:///taskmanager.db", echo=True)
+
+session_local = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
 
