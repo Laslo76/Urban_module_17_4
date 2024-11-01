3016b60c83959d2eab02f14ae106214c2bb0b85c
Laslo76 <162439161+Laslo76@users.noreply.github.com>
2024-11-01 08:42:22 +0300

    Add files via upload

--- add/migrations/version/2f47e1a8f311_second.py
+++ add/migrations/version/2f47e1a8f311_second.py
@@ -1,1 +1,26 @@
+"""second
 
+Revision ID: 2f47e1a8f311
+Revises: d888f371f227
+Create Date: 2024-10-31 21:00:29.271906
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '2f47e1a8f311'
+down_revision: Union[str, None] = 'd888f371f227'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
